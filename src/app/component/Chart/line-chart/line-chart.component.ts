ca034ca07cc3dd990778f264219ec3c4aab1c076
minhnt-int <minh.nt172684@sis.hust.edu.vn>
2023-10-25 16:32:09 +0700

    25-10 update

--- src/app/component/Chart/line-chart/line-chart.component.ts
+++ src/app/component/Chart/line-chart/line-chart.component.ts
@@ -5,15 +5,10 @@ import {
   AfterViewInit,
   OnChanges,
   SimpleChanges,
+  Input,
 } from '@angular/core';
 
 import * as d3 from 'd3';
-import * as d3select from 'd3-selection';
-import * as d3Scale from 'd3-scale';
-import * as d3Shape from 'd3-shape';
-import * as d3Array from 'd3-array';
-import * as d3Axis from 'd3-axis';
-import { ticker } from 'src/app/extra/data';
 
 @Component({
   selector: 'app-line-chart',
@@ -23,6 +18,9 @@ import { ticker } from 'src/app/extra/data';
   styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
+  @Input() data!: any;
+  @Input() comWidth: number = 900;
+  @Input() comHeight: number = 500;
   title = 'Line Chart';
 
   margin = { top: 20, right: 20, bottom: 30, left: 50 };
@@ -31,36 +29,28 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
   x: any;
   y: any;
   svg: any;
-  line?: d3Shape.Line<[number, number]>;
+  line?: d3.Line<[number, number]>;
   xAxis: any;
   yAxis: any;
   chartContainer: any;
   g: any;
   constructor() {
-    this.width = 900 - this.margin.left - this.margin.right;
-    this.height = 500 - this.margin.top - this.margin.bottom;
+    this.width = this.comWidth - this.margin.left - this.margin.right;
+    this.height = this.comHeight - this.margin.top - this.margin.bottom;
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges): void {
     this.initSvg();
     this.initAxis();
     this.drawAxis();
     this.drawLine();
     this.initZoom();
-    console.log(this.svg);
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    // this.initSvg();
-    // this.initAxis();
-    // this.drawAxis();
-    // this.drawLine();
-  }
-  ngAfterViewInit(): void {
-    console.log(this.margin);
-  }
+  ngAfterViewInit(): void {}
   initSvg() {
-    this.svg = d3select.select('#svg');
-
+    this.svg = d3.select('#svg');
+    this.svg.attr('width', this.comWidth).attr('height', this.comHeight);
     this.g = this.svg
       .append('g')
       .attr(
@@ -69,42 +59,36 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
       );
 
     this.chartContainer = this.g.append('g').attr('class', 'chart-container');
-    console.log(this.chartContainer, this.svg);
-
-    // svg.selectAll(".bars rect").attr("x", d => x(d.letter)).attr("width", x.bandwidth());
   }
 
   initAxis() {
-    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.x = d3.scaleTime().range([0, this.width]);
     // this.x = D3.scaleBand()
-    //   .domain(D3.sort(ticker, (d) => -d.close).map((d) => d.date.toISOString()))
+    //   .domain(D3.sort(this.data, (d) => -d.close).map((d) => d.date.toISOString()))
     //   .range([this.margin.left, this.width])
     //   .padding(0.1);
-    let low = d3.min(ticker, (d) => d.low);
-    let high = d3.max(ticker, (d) => d.high);
+    let low = d3.min(this.data, (d: any) => d.low as number);
+    let high = d3.max(this.data, (d: any) => d.high as number);
     this.y = d3
       .scaleLog()
       .domain([low ? low : 0, high ? high : 0])
       .rangeRound([this.height, this.margin.top]);
     this.x.domain(
-      d3Array.extent(ticker, (d) => {
-        console.log(d.date);
-
+      d3.extent(this.data, (d: any) => {
         return d.date;
       })
     );
-    // this.y.domain(d3Array.extent(ticker, (d) => d.value));
   }
 
   drawAxis() {
-    this.xAxis = d3Axis.axisBottom(this.x);
+    this.xAxis = d3.axisBottom(this.x);
     this.g
       .append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + this.height + ')')
       .call(this.xAxis);
 
-    this.yAxis = d3Axis.axisLeft(this.y);
+    this.yAxis = d3.axisLeft(this.y);
     this.g
       .append('g')
       .attr('class', 'axis axis--y')
@@ -119,7 +103,7 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
   }
 
   drawLine() {
-    this.line = d3Shape
+    this.line = d3
       .line()
       .x((d: any) => this.x(d.date))
       .y((d: any) => this.y(d.close));
@@ -129,7 +113,7 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
       .attr('stroke-linecap', 'round')
       .attr('stroke', 'black')
       .selectAll('g')
-      .data(ticker)
+      .data(this.data)
       .join('g')
       .attr(
         'transform',
@@ -141,6 +125,7 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
       .attr('y2', (d: any) => this.y(d.high));
 
     g.append('line')
+      .attr('class', 'line-point')
       .attr('y1', (d: any) => this.y(d.open))
       .attr('y2', (d: any) => this.y(d.close))
       .attr('stroke-width', 4)
@@ -154,7 +139,7 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
 
     this.chartContainer
       .append('path')
-      .datum(ticker)
+      .datum(this.data)
       .attr('class', 'line')
       .attr('d', this.line)
       .style('stroke-width', 3);
@@ -167,7 +152,6 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
         this.height + this.margin.bottom + this.margin.top,
       ],
     ];
-    console.log(this.height, this.margin.bottom, this.margin.top);
 
     this.svg.call(
       d3
@@ -176,17 +160,10 @@ export class LineChartComponent implements OnInit, AfterViewInit, OnChanges {
         .translateExtent(extent)
         .extent(extent)
         .on('zoom', (event) => {
-          console.log(event.transform);
           this.x.range([0, this.width].map((d) => event.transform.applyX(d)));
           this.g.select('.axis--x').call(this.xAxis);
 
-          let low = d3.min(ticker, (d) => d.low);
-          let high = d3.max(ticker, (d) => d.high);
-          console.log(this.y);
-
           this.y.range([0, this.width].map((d) => event.transform.applyY(d)));
-
-          // this.y.range([0, this.height].map((d) => event.transform.applyX(d)));
           this.g.select('.axis--y').call(this.yAxis);
           this.chartContainer.attr('transform', event.transform);
         })
